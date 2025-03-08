6e6effac2a86495653204d0f894e44f3717ca05d
sleeperops <gabrieljohnmikhaelh@gmail.com>
2025-03-08 13:43:10 +0800

    Updated to be compatible with the batch 2 programs
    updated "function_dict", now uses "function_names"  to display the functions for the user
    
    updated "input_a" and "input_b" variables, now uses "first_input" and "second_input" since that is the variable name used by the parameters of the ten programs.

--- ten_in_one_batch2.py
+++ ten_in_one_batch2.py
@@ -13,15 +13,15 @@ function_names = ['Prog 1: Print the smaller of 2 numbers',
                   'Prog 9: All even numbers from 0 - 100',
                   'Program 10: All numbers from 0-100 except those ending in zero']
 
-function_list = [greater_number, compare_ab, sum_ab, product_ab, quotient_ab, exponent_ab, summate, odd_numbers,
-                 all_even, no_zero]
+function_list = []
 
 # Selection: part where the user selects a program to play------------------------------------
+# Displays the function itself----
 while True:
-    for function in function_dict:  # displays all the function
+    for function in function_names:  # displays all the function
         print(function)
-    # function selection
-    print('')
+#Function selection----------------
+    print('') # space
     user_input = input('Pick the number of the program of your choice (// to exit): ')
     if user_input == '//':  # exit program
         break
@@ -30,7 +30,7 @@ while True:
     except:
         print('>>Not a number')
         continue
-#----------------------------------------------------------------------------------------------
+# executes the selected function----------------------------------------------------------------
     user_input -= 1  # to deal with 0 base
     if user_input >= 0 and user_input < 6:  # for functions with special parameters
         first_input = input('Enter the first number: ')
@@ -42,14 +42,14 @@ while True:
         except:
             print('>>Not numbers')
             continue
-        print(f'Answer: {function_list[user_input](input_a, input_b)}')  # calls the function based on user input
+        print(f'Answer: {function_list[user_input](first_input, second_input)}')  # calls the function based on user input
 
     elif user_input > 5 and user_input <= 10:  # for functions with no special parameters
         function_list[user_input]()  # calls the function based on user input
 
     else: # error in input
         print('>>Out of index')
-#---------------------------------------------------------------------------------------
+# exit / continue program-------------------------------------------------------------------------
     # exit / continue program
     to_continue = input("Continue? (any key or 'n' to exit): ")
     if to_continue.lower() == 'n':
